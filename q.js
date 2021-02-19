aabf2340c4ed14f4143313bf7eded12b8b3a0d56
Abdulrahman Al-Qabandi <al-qabandi@hotmail.com>
2021-02-19 01:36:25 -0800

    Update q.js

--- q.js
+++ q.js
@@ -1,8 +1,1 @@
-	function randomIntFromInterval(min,max)
-{//stackoverflow#4959975<3
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-		
-if(navigator.userAgent.indexOf("Chrome") != -1 ){//Chrome
-				
-}
+alert(1)
